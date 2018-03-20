8eaef6d9849eeb426771ceead3698f6131220b47
Mohammed Alshaboti <alshaboti.it@gmail.com>
2018-03-20 19:55:29 +1300

    Update run_gnmi.sh
    update json file

--- json/run_gnmi.sh
+++ json/run_gnmi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo env PATH=/home/shab/bin:/home/shab/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/local/go/bin:/usr/local/go/bin:/usr/local/go/bin:/home/shab/go/bin gnmi_set -ca=../cert/client/ca.crt -cert=../cert/client/client.crt -key=../cert/client/client.key -target_name=www.example.com -target_addr=192.168.3.10:8080 -replace=/:@faucet-configuration.json
+sudo env PATH=/home/shab/bin:/home/shab/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/local/go/bin:/usr/local/go/bin:/usr/local/go/bin:/home/shab/go/bin gnmi_set -ca=../cert/client/ca.crt -cert=../cert/client/client.crt -key=../cert/client/client.key -target_name=www.example.com -target_addr=192.168.3.10:8080 -replace=/:@simple-faucet-configuration.json
 
 
